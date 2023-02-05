36622f287a47cfdb65c1d8ea6697793c232f537c
priyanka387 <priyankapathak222@gmail.com>
2023-02-05 17:56:08 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Priyanka Kumari",
+    author_email="priyankapathak222@gmail.com",
+    packages=find_packages()
+)
